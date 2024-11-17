b5eafdc8bfef75eacb46d0e2bf819e44ea31346d
James Morgan <thehoracle@Jamess-MacBook-Pro.local>
2024-11-18 00:40:18 +0100

    test

--- src/enpoints/webhooks/paystack.ts
+++ src/enpoints/webhooks/paystack.ts
@@ -20,7 +20,7 @@ const PaystackWebhook: Endpoint = {
         // const signature = req.headers.get('x-paystack-signature');
         const ip = req.headers.get('x-forwarded-for')
         // if (!signature || hash !== signature) {
-        console.log('ip', ip)
+        console.log('ip: ', ip, 'status: ', PAYSTACK_IPS.includes(ip!))
         if (!ip || !PAYSTACK_IPS.includes(ip)) {
             return Response.json(
                 { error: 'Forbidden IP address, hehehe' },

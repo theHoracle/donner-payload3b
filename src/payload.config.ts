3b30d213bde604a905d3de9bc203490b90995c21
theHoracle <princemjames2@gmail.com>
2024-11-29 14:49:33 +0100

    updated config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -69,6 +69,7 @@ export default buildConfig({
         },
         endpoint: process.env.SUPABASE_ENDPOINT!,
         region: process.env.SUPABASE_REGION,
+        forcePathStyle: true,
         // ... Other S3 configuration
       },
     }),
